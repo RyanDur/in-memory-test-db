ba7e00809e61157b4d45a511593e871030a0cf84
RyanDur <ryan.p.durling@gmail.com>
2016-12-24 20:06:03 -0600

    add db schema

---
+++ programmers-and-unicorns/src/main/resources/migration/V1__base.sql
@@ -0,0 +1,33 @@
+CREATE TABLE games(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE boards(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  game_id INT NOT NULL,
+  CONSTRAINT games_id_fk
+  FOREIGN KEY (game_id)
+  REFERENCES games(id)
+);
+
+CREATE TABLE ships(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  type VARCHAR(50),
+  start INT NOT NULL,
+  end INT NOT NULL,
+  sunk BOOL DEFAULT FALSE,
+  ship_board_id INT NOT NULL,
+  CONSTRAINT ship_boards_id_fk
+  FOREIGN KEY (ship_board_id)
+  REFERENCES boards(id)
+);
+
+CREATE TABLE moves(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  point INT NOT NULL,
+  status VARCHAR(4) DEFAULT 'MISS',
+  move_board_id INT NOT NULL,
+  CONSTRAINT move_boards_id_fk
+  FOREIGN KEY (move_board_id)
+  REFERENCES boards(id)
+)
