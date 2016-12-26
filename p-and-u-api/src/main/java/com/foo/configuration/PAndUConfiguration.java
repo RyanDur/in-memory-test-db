2d19f313784b1f102c65e0db46390288d19afe85
RyanDur <ryan.p.durling@gmail.com>
2016-12-26 09:36:40 -0600

    working

--- p-and-u-api/src/main/java/com/foo/configuration/PAndUConfiguration.java
+++ p-and-u-api/src/main/java/com/foo/configuration/PAndUConfiguration.java
@@ -1,25 +1,9 @@
 package com.foo.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan({"com.db"})
 public class PAndUConfiguration {
-
-    @Bean
-    public DataSource getDataSource(@Value("${spring.datasource.url}") String url,
-                                    @Value("${spring.datasource.username}") String username,
-                                    @Value("${spring.datasource.driver-class-name}") String driver) {
-        return DataSourceBuilder.create()
-                .url(url)
-                .username(username)
-                .driverClassName(driver)
-                .build();
-    }
 }
