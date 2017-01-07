f25dd8835b9a73c65de8e4cea9d28730b458e98d
RyanDur <ryan.p.durling@gmail.com>
2017-01-07 16:14:46 -0600

    disable flyway

--- p-and-u-api/src/main/java/com/foo/PAndUApiApplication.java
+++ p-and-u-api/src/main/java/com/foo/PAndUApiApplication.java
@@ -1,9 +1,12 @@
 package com.foo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude=FlywayAutoConfiguration.class)
 public class PAndUApiApplication {
 
     public static void main(String[] args) {
