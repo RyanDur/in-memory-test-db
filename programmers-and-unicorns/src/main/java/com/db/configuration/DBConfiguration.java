723491d713db8b23adc8914ba76d838e1875554f
RyanDur <ryan.p.durling@gmail.com>
2017-01-07 13:49:14 -0600

    foo

--- programmers-and-unicorns/src/main/java/com/db/configuration/DBConfiguration.java
+++ programmers-and-unicorns/src/main/java/com/db/configuration/DBConfiguration.java
@@ -1,19 +1,24 @@
 package com.db.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DBConfiguration {
-//
-//    @Bean
-//    public DataSource getDataSource(@Value("${spring.datasource.url}") String url,
-//                                    @Value("${spring.datasource.username}") String username,
-//                                    @Value("${spring.datasource.driver-class-name}") String driver) {
-//
-//        return DataSourceBuilder.create()
-//                .url(url)
-//                .username(username)
-//                .driverClassName(driver)
-//                .build();
-//    }
+
+    @Bean
+    public DataSource getDataSource(@Value("${spring.datasource.url}") String url,
+                                    @Value("${spring.datasource.username}") String username,
+                                    @Value("${spring.datasource.driver-class-name}") String driver) {
+
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .driverClassName(driver)
+                .build();
+    }
 }
