dfa35a6d4363f4c8e615361fed6be8393a1f4d27
Paul Pelafas and Ryan Durling <pair+paul+rdurling@pivotal.io>
2017-01-12 14:01:18 -0600

    update pipeline

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -8,4 +8,4 @@ pushd post-build-pu-codebase/
   ./gradlew clean build &&
 popd
 
-mv post-build-pu-codebase/p-an-u-api/build/libs/p-and-u-api-*.jar pu-jars/
+mv post-build-pu-codebase/p-and-u-api/build/libs/p-and-u-api-*.jar pu-jars/
